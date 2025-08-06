58abfb556cf39616e890a536845bd1110386cde9
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-08-06 14:22:20 +0500

    update

---
+++ Chapter_4/practice.py
@@ -0,0 +1,28 @@
+# # list = ["akash", "ajay"]
+# # print(list[0])
+
+# # list[0] = "sharukh"
+
+# # list.append("Harry")
+# # print(list[0:])
+
+# l1= [1, 9, 3, 5, 2 , 89, 33, 21]
+# # l1.sort()
+# # l1.reverse()
+# # l1.insert(1, 2)
+# print(l1)
+# l1.pop(1)
+# print(l1)
+
+#################################################
+
+#-------------------TUPLE NOW---------------------------------------
+
+a= (1, 2, 1, 5, 6)
+
+print(a)
+# b = (1,)
+# print(type(b))
+# b= a.count(1)
+b= a.index(5)
+print(b)
---
+++ Chapter_4/problem_2.py
@@ -0,0 +1,14 @@
+marks = []
+ 
+f1 = int(input("Enter the marks:    "))
+marks.append(f1)
+f2 = int(input("Enter the marks:   "))
+marks.append(f2)
+f3= int(input("Enter the marks:  "))
+marks.append(f3)
+f4 = int(input("Enter the marks:  "))
+marks.append(f4)
+
+print(marks)
+marks.sort()
+print(marks)

40dba8c7fc50f04f267e1aef4ff52dfbb2f81516
Umair Khalil <98485634+umairkhalil189@users.noreply.github.com>
2025-08-10 18:00:27 +0500

    update

---
+++ Chapter_11/practice.py
@@ -0,0 +1,34 @@
+class employee:
+    company = "ITC"
+    name = "harry"
+
+    def show(self):
+        print(f"The employee name is:    {self.name} and the company is {self.company}")
+
+
+# class programmer:
+#     company = "ITC infotech"
+
+#     def show(self):
+#         print(f"The name is:    {self.name} and the salary is {self.salary}")
+
+#     def showLanguage(self):
+#         print(f"The name is:    {self.name} and he is good with {self.language} language")
+
+class coder:
+    language = "Python"
+    def print_languages(self):
+        print(f"Out of all the languages, here is your language {self.language}")
+
+class programmer(employee, coder):
+    company = "ITC infotech"
+
+    def showLanguage(self):
+        print(f"The company name is:    {self.company} and they are good with {self.language} language")
+
+a= employee()
+b= programmer()
+
+b.show()
+b.print_languages()
+b.showLanguage()
